b8050ccc72ac428f0558c64ef369875a6fb2f450
Antoine Bidaud <antoine.bidaud@yahoo.fr>
2021-10-08 17:28:00 +0200

    Add test for two/three + prod code

--- Yahtzee/Models/Game.cs
+++ Yahtzee/Models/Game.cs
@@ -21,5 +21,38 @@ namespace Yahtzee.Models
 
             return result;
         }
+
+        public object CalculateTwos(int[] roll)
+        {
+            var result = 0;
+
+            foreach (var item in roll)
+            {
+                if (item == 2)
+                {
+                    result = result + 2;
+                }
+
+            }
+
+            return result;
+        }
+
+        public object CalculateThrees(int[] roll)
+        {
+            var result = 0;
+            foreach (var item in roll)
+            {
+
+                if(item == 3)
+                {
+                    result = result + 3;
+                }
+
+            }
+
+            return result;
+        }
+
     }
 }
--- YahtzeeTests/GameTests.cs
+++ YahtzeeTests/GameTests.cs
@@ -7,21 +7,6 @@ namespace YahtzeeTests
     public class GameTests
     {
 
-        [Fact]
-        public void DiceSuccessfullCreated()
-        {
-            var dice1 = new Dice();
-            var dice2 = new Dice();
-            var dice3 = new Dice();
-            var dice4 = new Dice();
-            var dice5 = new Dice();
-
-            var roll = new Roll(dice1, dice2, dice3, dice4, dice5);
-
-            Assert.NotNull(roll);
-            
-        }
-
         [Theory]
         [InlineData(new[] { 1, 2, 3, 4, 5 }, 1)]
         [InlineData(new[] { 1, 2, 1, 4, 5 }, 2)]
@@ -35,6 +20,39 @@ namespace YahtzeeTests
             Assert.Equal(expectedScore, score);
 
         }
+
+
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 2)]
+        [InlineData(new[] { 1, 2, 1, 4, 5 }, 2)]
+        [InlineData(new[] { 1, 2, 1, 4, 1 }, 2)]
+        [InlineData(new[] { 2, 2, 3, 4, 5 }, 4)]
+        public void CalculatedTwos(int[] roll, int expectedScore)
+        {
+
+            var game = new Game();
+            var score = game.CalculateTwos(roll);
+
+            Assert.Equal(expectedScore, score);
+        }
+
+        [Theory]
+        [InlineData(new[] { 3, 2, 3, 4, 5 }, 6)]
+        [InlineData(new[] { 1, 2, 1, 4, 5 }, 0)]
+        [InlineData(new[] { 1, 2, 1, 3, 1 }, 3)]
+        [InlineData(new[] { 2, 2, 3, 3, 3 }, 9)]
+        public void CalculatedThrees(int[] roll, int expectedScore)
+        {
+
+            var game = new Game();
+            var score = game.CalculateThrees(roll);
+
+            Assert.Equal(expectedScore, score);
+        }
+
+
+        
+
 
     }
 }
